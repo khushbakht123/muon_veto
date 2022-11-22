5ef3c05713857e2003ecbf64c11930a01a676dc3
khushbakht123 <khushihabib67@gmail.com>
2022-11-22 01:03:00 -0800

    trigger_thr file added

---
+++ trigger_thr.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+import numpy as np
+from pygama.dsp.errors import DSPFatal
+
+from numba import guvectorize
+
+from pygama.dsp.utils import numba_defaults_kwargs as nb_kwargs
+
+@guvectorize(
+    ["void(float32[:], float32, float32[:])"], 
+#    ["void(float32[:], float32, int32[:])"],
+    "(n),()->(n)",
+    **nb_kwargs,
+)
+
+def trigger_thr(heights: np.ndarray, value: float, triggered: np.ndarray) -> None:
+      
+    
+    triggered[:] = np.nan
+ 
+    if np.isnan(heights).any() or np.isnan(value):
+        return
+    
+    
+    triggered = np.zeros(len(heights))
+#    triggered[0]=heights[0]
+    
+
+    for i in range(0,len(heights), 1):
+        
+        if(heights[i] > value):
+            triggered[i]=1.0
